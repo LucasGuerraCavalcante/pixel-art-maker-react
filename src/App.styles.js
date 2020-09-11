e8c07c3af22f4a76fda07da367d051cd806ff650
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-09-11 14:20:36 -0300

    app styles

---
+++ src/App.styles.js
@@ -0,0 +1,11 @@
+import { createUseStyles } from 'react-jss';
+
+export default createUseStyles({
+  app: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
