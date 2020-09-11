ab64d0f6c6ffd30f80ff6e1aa066e84a7672ab8d
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-09-11 17:30:14 -0300

    props and tool box

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Grid from './components/Grid/';
 import ColorPicker from './components/ColorPicker/';
 import useStyles from './App.styles';
 
 function App() {
+  const [currentColor, setCurrentColor] = useState('#56BC58');
+
   const classes = useStyles();
   return (
     <div className={classes.app}>
-      <ColorPicker />
-      <Grid />
+      <div className={classes.toolBox}>
+        <ColorPicker currentColor={currentColor} setCurrentColor={setCurrentColor} />
+        
+      </div>
+      <Grid currentColor={currentColor} />
     </div>
   );
 }
