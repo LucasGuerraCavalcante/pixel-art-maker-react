6fc0c85413c5bfe1cdd9153728ef5e10d43082cf
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-09-11 16:21:26 -0300

    flex direction column

--- src/App.styles.js
+++ src/App.styles.js
@@ -3,6 +3,7 @@ import { createUseStyles } from 'react-jss';
 export default createUseStyles({
   app: {
     display: 'flex',
+    flexDirection: 'column',
     width: '100%',
     height: '100%',
     justifyContent: 'center',
