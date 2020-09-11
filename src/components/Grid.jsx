46e403da7aa99e76b0ff6eefd28c409bbd7e9e0b
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-09-11 14:32:39 -0300

    Grid component

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,2 +1,14 @@
 import React from 'react';
 
+import useStyles from './Grid.styles'
+
+const Grid = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.grid}>
+
+        </div>
+    )
+};
+
+export default Grid;
