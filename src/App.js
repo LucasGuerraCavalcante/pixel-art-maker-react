383a9d3c8ae60f488c242090aef0b04bad401d1c
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-09-11 20:31:49 -0300

    grid switch button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
 
 import Grid from './components/Grid/';
+import GridSwitch from './components/GridSwitch/';
 import ColorPicker from './components/ColorPicker/';
 import useStyles from './App.styles';
 
 function App() {
-  const [currentColor, setCurrentColor] = useState('#56BC58');
+  const [currentColor, setCurrentColor] = useState();
+  const [showGrid, setShowGrid] = useState(false);
 
   const classes = useStyles();
   return (
     <div className={classes.app}>
       <div className={classes.toolBox}>
         <ColorPicker currentColor={currentColor} setCurrentColor={setCurrentColor} />
-        
+        <GridSwitch showGrid={showGrid} setShowGrid={setShowGrid} />
       </div>
-      <Grid currentColor={currentColor} />
+      <Grid currentColor={currentColor} showGrid={showGrid} />
     </div>
   );
 }
