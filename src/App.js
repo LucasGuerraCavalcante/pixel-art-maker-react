c720ca50ed43d557e58fed8ce547a89140db0d85
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-09-11 22:30:32 -0300

    color palette

--- src/App.js
+++ src/App.js
@@ -3,20 +3,44 @@ import React, { useState } from 'react';
 import Grid from './components/Grid/';
 import GridSwitch from './components/GridSwitch/';
 import ColorPicker from './components/ColorPicker/';
+import ColorPalette from './components/ColorPalette/';
 import useStyles from './App.styles';
 
 function App() {
-  const [currentColor, setCurrentColor] = useState();
+  const [currentColor, setCurrentColor] = useState('#000');
+  const [colorHistoryPalatte, setColorHistoryPalatte] = useState(['#000']);
   const [showGrid, setShowGrid] = useState(false);
+
+  const onSetColor = (color) => {
+    setColorHistoryPalatte(colorHistoryPalatte.slice(-5).concat(color))
+    setCurrentColor(color);
+  };
 
   const classes = useStyles();
   return (
     <div className={classes.app}>
+
       <div className={classes.toolBox}>
-        <ColorPicker currentColor={currentColor} setCurrentColor={setCurrentColor} />
-        <GridSwitch showGrid={showGrid} setShowGrid={setShowGrid} />
+        <ColorPicker 
+          currentColor={currentColor} 
+          onSetColor={onSetColor}
+        />
+
+        <GridSwitch 
+          showGrid={showGrid} 
+          setShowGrid={setShowGrid} 
+        />
       </div>
-      <Grid currentColor={currentColor} showGrid={showGrid} />
+
+      <ColorPalette 
+          colorHistoryPalatte={colorHistoryPalatte} 
+          onSetColor={onSetColor} 
+      />
+
+      <Grid 
+        currentColor={currentColor} 
+        showGrid={showGrid} 
+      />
     </div>
   );
 }
